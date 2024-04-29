02d60dd0f6cd127e9a2fa59da02ca503d69a44c5
Aryo Bagas Sasongko <106600608+IAmBaghas@users.noreply.github.com>
2024-04-30 06:33:17 +0700

    Update efs.sh

--- efs.sh
+++ efs.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# Install necessary packages
 sudo apt-get update
-sudo apt-get install -y nfs-common jq
+sudo apt-get install -y binutils
+git clone https://github.com/aws/efs-utils
+cd efs-utils
+sudo chmod -Rv 700 /home/ubuntu/efs-utils/build/
 
-# Retrieve EFS file system ID and region
-efs_id=$(aws efs describe-file-systems --query "FileSystems[0].FileSystemId" --output text)
-efs_region=$(aws efs describe-file-systems --query "FileSystems[0].AvailabilityZone" --output text | cut -d'-' -f1)
+sudo apt-get install -y binutils gcc make
+sudo apt-get install -y libssl-dev libcurl4-openssl-dev libfuse-dev
 
-# Create mount point if it doesn't exist
-sudo mkdir -p /var/www/efs-mount
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
 
-# Check if mount point exists
-if [ ! -d "/var/www/efs-mount" ]; then
-    echo "Error: Failed to create mount point /var/www/efs-mount"
-    exit 1
-fi
-
-# Mount EFS file system
-sudo mount -t nfs4 ${efs_id}.efs.${efs_region}.amazonaws.com:/ /var/www/efs-mount
-
-# Optional: Add the mount command to /etc/fstab for automatic mounting on boot
-echo "${efs_id}.efs.${efs_region}.amazonaws.com:/ /var/www/efs-mount nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0" | sudo tee -a /etc/fstab
+./build-deb.sh
+sudo apt-get install -y ./build/amazon-efs-utils*deb
