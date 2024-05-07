d43e754c3005ca02753e1e831ea699cb3e653e08
Aryo Bagas Sasongko <106600608+IAmBaghas@users.noreply.github.com>
2024-05-07 08:23:05 +0700

    ngings

---
+++ nginx.sh
@@ -0,0 +1,6 @@
+apt-get install nano software-properties-common python3-software-properties -y
+apt-get update -y
+apt-get install nginx mysql-client mysql-server php -y
+sudo apt install graphviz aspell ghostscript clamav php8.1-pspell php8.1-curl php8.1-gd php8.1-intl php8.1-mysql php8.1-xml php8.1-xmlrpc php8.1-ldap php8.1-zip php8.1-soap php8.1-mbstring -y
+/etc/init.d/nginx status
+/etc/init.d/nginx start
