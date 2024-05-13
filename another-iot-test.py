d2fed507739a750e59b2631c4369831667d46e9a
Aryo Bagas Sasongko <106600608+IAmBaghas@users.noreply.github.com>
2024-05-14 06:08:05 +0700

    Create another-iot-test.py

---
+++ another-iot-test.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+# Function to read temperature from sensor
+def read_temperature():
+    # Your code to read temperature goes here
+    temperature = 25.0  # This is just a placeholder
+    return temperature
+
+# Function to publish temperature to AWS IoT Core
+def publish_temperature(temperature):
+    command = f'mosquitto_pub -h <your-endpoint>.iot.<region>.amazonaws.com -p 8883 --cafile <path-to-rootCA.pem> --cert <path-to-certificate.pem.crt> --key <path-to-private.pem.key> -t <your-topic> -m "{temperature}"'
+    os.system(command)
+
+# Main function
+def main():
+    temperature = read_temperature()
+    publish_temperature(temperature)
+
+if __name__ == "__main__":
+    main()
